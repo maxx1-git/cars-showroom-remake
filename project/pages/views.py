13860955b077cc947bf59436f8cba752b052a920
max <polamaxp@gmail.com>
2026-02-23 22:19:47 +0200

    Fixed Index Page Featured Cars urls | Created a dynamic system for Featured Cars that shows latest 3 cars added to showroom dynamicly

--- project/pages/views.py
+++ project/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from showroom.models import Car
 # Create your views here.
 
 # def index(request):
@@ -8,6 +9,13 @@ from django.views import generic
 
 class Index(generic.TemplateView):
     template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['featured_cars'] = Car.objects.order_by('-created_at')[:3]
+        return context
+
+
 
 
 class Contact(generic.TemplateView):
